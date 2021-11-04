6200745cbb650d2358222f75b6bca88006a77062
karyakirana <adibpratama2@gmail.com>
2021-11-04 14:05:53 +0700

    first commit

---
+++ resources/views/try.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<title>My First Coding</title>
+<link rel="stylesheet" href="/app.css"> 
+
+<body>
+    <article>
+    
+        <h1><a href="/try">My First Coding</a></h1>
+
+        <p>
+        bekerja demi masa depan yang indah brodie
+        </p>
+    </article>
+
+        <a href="/">Go Back</a>
+
+</body>
+
---
+++ resources/views/trying.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<title>My First Coding</title>
+<link rel="stylesheet" href="/app.css"> 
+
+<body>
+    <article>
+    
+        <h1><a href="/try">My First Coding</a></h1>
+
+        <p>
+        bekerja demi masa depan yang indah brodie
+        </p>
+    </article>
+
+    <article>
+    
+        <h1><a href="/try">My Second Coding</a></h1>
+
+        <p>
+        terus semangat agar menjadi manusia diatas rata-rata 
+        </p>
+    </article>
+
+    <article>
+    
+        <h1><a href="/try">My Third Coding</a></h1>
+
+        <p>
+        kebaikan selalu datang di waktu yang tepat
+        </p>
+    </article>
+
+
+</body>
+
